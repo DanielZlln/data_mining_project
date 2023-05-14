acb93901c561009a9fd0de4f4d9f0b6500a8bba3
John Doe <daniel.z.1995@web.de>
2023-05-14 11:19:41 +0200

    final version

--- Final.R
+++ Final.R
@@ -34,6 +34,11 @@ tidymodels_prefer()
 ### Daten einlesen
 load("heart_disease.RData")
 
+
+#-----------------------------------------------------------------------------
+# Datenaufbereitung, Ueberblick verschaffen
+#------------------------------------------------------------------------------
+
 str(heart_disease)
 summary(heart_disease)
 
@@ -49,6 +54,7 @@ summary(heart_disease)
 introduce(heart_disease)
 plot_intro(heart_disease)
 
+# wie viele falsche Werte liegen im Datensatz?
 heart_disease %>% 
   filter(Hoher_Cholspiegel > 1 | Hoher_Blutdruck > 1 |
            Rauchen > 1 | Phys_Aktivität > 1) %>% 
@@ -56,13 +62,11 @@ heart_disease %>%
   summarise(anzahl = n())
 # Insgeasmt 16 Zeilen mit falschen Werten
 
-
 # Herzinfarkt
 heart_disease %>% 
   group_by(Herzinfarkt) %>% 
   summarise(anzahl = n())
-# 2 Zeilen "unbekannt" löschen
-
+# 2 Zeilen "unbekannt" loeschen
 
 # Infarkt
 mean(heart_disease$Herzinfarkt == "Infarkt") 
@@ -102,7 +106,7 @@ heart_disease %>%
 
 # Bildung = unbekannt, löschen da nur 4 Einträge
 
-# Zeilen löschen die falsche Werte enthalten
+# Zeilen loeschen die falsche Werte enthalten
 heart_disease <- heart_disease %>% 
   filter(Alter != "unbekannt")
 
@@ -121,6 +125,7 @@ heart_disease <- heart_disease %>%
 heart_disease <- heart_disease %>% 
   filter(Phys_Aktivität <= 1)
 
+
 heart_disease <- heart_disease %>% 
   filter(Herzinfarkt != "unbekannt")
 
@@ -137,6 +142,7 @@ heart_disease <- heart_disease %>%
 boxplot(heart_disease$BMI~heart_disease$Alter) # Wieso so viele Ausreiser nach oben?
 mean(heart_disease$BMI>30, na.rm = T) 
 
+# Histogramm erstellen
 h = hist(heart_disease$BMI, breaks = 100)
 ccat = cut(h$breaks, c(-Inf, 30, Inf))
 plot(h, col=c("white","red")[ccat])
@@ -153,10 +159,12 @@ table(Gesundheit = heart_disease$Allg_Gesundheit,
                       breaks = c(0,10,20,30,40,50,60,Inf),
                       dig.lab = 5,
                       include.lowest = T))
+
 # Gesundheitsstatus gut -> mehr kleine Werte
 
-# ------------------------------------------------------------------------
-
+#-----------------------------------------------------------------------------
+# Datenaufbereitung, Ueberblick verschaffen
+#------------------------------------------------------------------------------
 
 # Datensatz aufteilen zwischen test und train
 set.seed(123)
@@ -182,8 +190,6 @@ heart_test %>%
   count(Herzinfarkt) %>% 
   mutate(prop = n/sum(n))
 
-
-
 # Create recipe
 heart_recipe_down <- 
   recipe(Herzinfarkt ~., data = heart_train) %>% 
@@ -206,13 +212,14 @@ heart_recipe_down_num <- heart_recipe_down %>%
 #              one_hot = TRUE)
 
 recipes <- list(lr_recipe = heart_recipe_down)
-                #lr_tune_recipe = heart_recipe_down_num)
-                #dt_recipe = heart_recipe_down)
-                #rf_recipe = heart_recipe_down,
-                #xgb_recipe = heart_recipe_down_num)
+#lr_tune_recipe = heart_recipe_down_num)
+#dt_recipe = heart_recipe_down)
+#rf_recipe = heart_recipe_down,
+#xgb_recipe = heart_recipe_down_num)
 
+#-------------------------------------------------------------------------------
 # Resampling
-# ------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
 # Cross Validation
 set.seed(123)
@@ -220,9 +227,9 @@ heart_folds <- vfold_cv(heart_train)
 heart_folds
 
 
-
+#------------------------------------------------------------------------------
 # Modeling
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 
 # Logistic Regression
@@ -251,7 +258,7 @@ rf_model <-
               engine = "ranger",
               trees = tune(),
               min_n = tune())
-              #mtry = tune()) wird nicht erkannt
+#mtry = tune()) wird nicht erkannt
 
 # XGBoost
 xgb_model <- 
@@ -270,10 +277,10 @@ xgb_model <-
 
 # List of models
 models <- list(lr_model = lr_model)
-               #lr_model_tune = lr_mode_tune)
-               #dt_model = dt_model,
-               #rf_model = rf_model)
-               #xgb_model = xgb_model)
+#lr_model_tune = lr_mode_tune)
+#dt_model = dt_model,
+#rf_model = rf_model)
+#xgb_model = xgb_model)
 
 # Create workflow set
 heart_workflows <- workflow_set(preproc = recipes,
@@ -336,23 +343,22 @@ pred %>%
 # falls die Kurven stark voneinander abweichen, muss man von overfitting 
 # ausgehen - hier weichen die Kurven nicht voneinander ab
 
+#------------------------------------------------------------------------------
 # Evaluation
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 collection <- collect_metrics(heart_fit, summarize = F)
 collection
 
-#
+# Accuracy und ROC AUC darstellen 
 autoplot(heart_fit,
          rank_metric = "roc_auc",
          metrich = "roc_auc",
          select_best = T)
 
-#
 heart_fit %>% rank_results(rank_metric = "roc_auc",
                            select_best = T) %>% 
   filter(.metric == "roc_auc")
 
-# 
 best_result <- 
   heart_fit %>% 
   extract_workflow_set_result("lr_recipe_lr_model") %>% 
@@ -370,14 +376,13 @@ best_test_results %>%
   collect_metrics()
 
 
-
+#-------------------------------------------------------------------------------
 # Prediction
+#-------------------------------------------------------------------------------
 
 best_test_results$.predictions
-# 
-# # ------------------------------------------------------------------------------
 
-### Prediction on test
+# Prediction on test
 
 heart_test %>% 
   select(Herzinfarkt) %>% 
@@ -387,7 +392,7 @@ heart_test %>%
                         type = "prob")) %>% 
   roc_auc(truth = Herzinfarkt, estimate = `.pred_Infarkt`)
 
-
+# Matrix erstellen 
 kosten_ertaege <- matrix(c(5000, -5000, -1000, 0), nrow = 2, ncol = 2)
 neu <- 0
 best_threshold <- 0
@@ -422,3 +427,73 @@ conf_erg <- heart_test %>%
                                            threshold = .47)) %>% 
   conf_mat(estimate = .pred_class, truth = Herzinfarkt)
 sum(conf_erg$table*kosten_ertaege)
+
+
+
+# Welche Variablen haben groessten einfluss
+
+best_result_fitted <- best_test_results$.workflow[[1]]
+
+lr_explain <- explain_tidymodels(
+  best_result_fitted,
+  data = dplyr::select(heart_disease, -Herzinfarkt),
+  y = abs(as.integer(heart_disease$Herzinfarkt)-1),
+  predict_function = function(lr_model, newdata)
+    pull(predict(lr_model,newdata, type = "prob")[".pred_Infarkt"]),
+  verbose = F
+)
+
+lr_vip <- lr_explain %>% 
+  model_parts(type = "variable_importance")
+
+head(lr_vip)
+
+plot(lr_vip)
+
+# BMI hat den groessten Einlfuss und kann mit prevention verbessert werden
+
+
+# heart disease result
+load("heart_disease_result.RData")
+str(heart_disease_result)
+summary(heart_disease_result)
+
+# Heart result so aufbereiten wie den alten
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Alter != "unbekannt")
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Geschlecht != "unbekannt")
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Hoher_Blutdruck <= 1)
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Hoher_Cholspiegel <= 1)
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Rauchen <= 1)
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Phys_Aktivität <= 1)
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Bildung != "unbekannt")
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(Einkommen != "unbekannt")
+
+heart_disease_result <- heart_disease_result %>% 
+  filter(complete.cases(.))
+
+
+# Vorhersage an neuen Datensatz
+heart_disease_result$predicted <- predict(lr_form_fit, 
+                                          heart_disease_result)
+
+# Bei wie vielen wir ein Infarkt vorhergesagt
+table(heart_disease_result$predicted)
+
+# Speichern RDATA
+save(heart_disease_result, file = "vorhersagen.RDATA")
